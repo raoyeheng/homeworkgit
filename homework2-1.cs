b4806aeb6222b53570702ed1af7ac35268700cf9
raoyeheng <1148696552@qq.com>
2020-03-02 00:16:59 +0800

    homework

---
+++ homework2-1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    
+    class Primefactor
+    {
+        bool Isprime(int n)
+        {
+            bool ret = true;
+            if (n == 2) { return ret; };
+            if (n % 2 != 0)
+            {
+                int bound = (int)System.Math.Sqrt(n) + 1;
+                for (int i = 2; i < bound; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        ret = false;
+                        break;
+                    }
+                }
+            }
+            else { ret = false; }
+            return ret;
+        }
+        private void display(int k)
+        {
+            Console.WriteLine("素数因子是：");
+            for(int n = 2; n < k+1; n++)
+            {
+                if (k % n == 0 && Isprime(n)==true)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Primefactor p1 = new Primefactor();
+            p1.display(6);
+        }
+    }
+     
+}
---
+++ homework2-3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homework8
+{
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        { 
+            void sieve(int n)
+            {
+                bool[] is_prime = new bool[0];
+                int[] prime = new int[0];
+                int t = 0;
+                for (int i = 0; i <=n; i++)
+                {
+                    Array.Resize<bool>(ref is_prime, is_prime.Length + 1); //动态增加数组长度
+
+                    is_prime[i] = true;
+                }
+                is_prime[0] = is_prime[1] = false;
+                for (int i = 2; i <= n; i++)//依次遍历
+                {
+                    if (is_prime[i])//检验到目前为止其是不是素数        
+                    {
+                        
+                        Array.Resize<int>(ref prime, prime.Length + 1);
+                        prime[t++] = i;
+                       
+                        for (int j = 2 * i; j <= n; j += i)
+                        {
+                            is_prime[j] = false;//其整数倍不是素数，置为false
+                        }
+                    }
+                }
+                for(int i = 0; i < t; i++)
+                {
+                    Console.WriteLine($"{prime[i]}");
+                }
+            }
+            sieve(100);
+        }
+    }
+}
